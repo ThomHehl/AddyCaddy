07edaae84d08c8ff3c017c76c7928e5957802b01
Thomas Hehl <thom@heavyweightsoftware.com>
2019-05-31 13:02:21 -0400

    Remove address validation

--- src/main/java/org/addycaddy/service/AddressValidationService.java
+++
@@ -1,8 +0,0 @@
-package org.addycaddy.service;
-
-import com.heavyweightsoftware.exception.HeavyweightException;
-import org.addycaddy.client.dto.ContactPointDto;
-
-public interface AddressValidationService {
-    ContactPointDto[] validate(ContactPointDto contactPointDto) throws HeavyweightException;
-}
